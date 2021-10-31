2750e0be24d458228082cefd22b532dbbfe0818c
heiung2001 <hieu.working2001@gmail.com>
2021-10-31 10:35:23 +0700

    build coefficient matrix method

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -7,8 +7,12 @@ namespace Week05
 {
     class CentralInterpolation
     {
-        private List<double> X;
-        private List<double> Y;
+        private List<double> X;     // mốc nội suy
+        private List<double> Y;     // giá trị nội suy
+        private List<double> above { get; }
+        private List<double> below { get; }
+        private List<double> Gauss { get; }
+        private double h => Math.Abs(X[0] - X[1]);
 
         public CentralInterpolation(List<double> X, List<double> Y)
         {
@@ -22,11 +26,59 @@ namespace Week05
             this.Y = lines[1].Split(" ").Select(y => double.Parse(y)).ToList();
         }
 
+        public List<double> Add_Above()
+        {
+            return new List<double> {0};
+        }
+        public List<double> Add_Below()
+        {
+            return new List<double> {0};
+        }
+
+        public void Construct_devided_diff(int idx)     // Chỉ số của x0 (mốc nội suy trung tâm)
+        {
+
+        }
+        public int[,] Construct_coeff_matrix(int numberOfX)
+        {
+            int n = numberOfX;
+            int[,] matrix = new int[n, n];
+            int[] x = new int[n];
+
+            matrix[0, n-1] = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if ((i+1) % 2 == 0)    { x[i] = (i+1) / 2;  }
+                if ((i+1) % 2 != 0)    { x[i] = -(i+1) / 2; }
+            }
+            
+            for (int i = 1; i < n; i++)
+            {
+                matrix[i, n-i-1] = 1;
+                for (int j = n-i; i < n-1; j++)
+                {
+                    matrix[i, j] = matrix[i-1, j+1] - x[i-1]*matrix[i-1, j];
+                }
+            }
+
+            return matrix;
+        }
+
+        public void Perform_Sterling()
+        {
+
+        }
+        public void Perform_Bessel()
+        {
+
+        }
+
         public void Display()
         {
             this.X.ForEach(x => Console.Write(x + " "));
             Console.Write("\n");
             this.Y.ForEach(y => Console.Write(y + " "));
+            Console.Write("\n" + h);
         }
     }
 
@@ -34,7 +86,7 @@ namespace Week05
     {
         static void Main(string[] args)
         {
-            string path = "input.txt";
+            string path = @".\input.txt";
             CentralInterpolation p = new CentralInterpolation(path);
             p.Display();
         }
