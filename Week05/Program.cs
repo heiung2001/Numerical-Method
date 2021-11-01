cf6f145d5f47dc87409fdc811d9a14e0c8192dc7
heiung2001 <hieu.working2001@gmail.com>
2021-11-01 23:37:32 +0700

    format code

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -7,12 +7,12 @@ namespace Week05
 {
     class CentralInterpolation
     {
-        private List<double> X;     // mốc nội suy
-        private List<double> Y;     // giá trị nội suy
+        private List<double> X = new List<double>();     // mốc nội suy
+        private List<double> Y = new List<double>();     // giá trị nội suy
+        private double h => Math.Abs(X[0] - X[1]);
         public List<double> above { get; private set; } = new List<double>();
         public List<double> below { get; private set; } = new List<double>();
         public List<double> Gauss { get; private set; } = new List<double>();
-        private double h => Math.Abs(X[0] - X[1]);
 
         public CentralInterpolation(List<double> X, List<double> Y)
         {
@@ -55,26 +55,55 @@ namespace Week05
             return temp;
         }
 
-        public void Perform_1st_Gauss(int idx)     // Chỉ số của x0 (mốc nội suy trung tâm)
+        public List<double> Perform_1st_Gauss(int idx)     // Chỉ số của x0 (mốc nội suy trung tâm)
         {
+            List<double> res = new List<double>();
+            int[,] t_table;
+            int k = 0;
+
             /* Ban đầu */
             above.Add(Y[idx]);
             below.Add(Y[idx]);
             Gauss.Add(Y[idx]);
 
             /* Thêm lần lượt các mốc nội suy */
-            for (int k = 1; k <= Math.Min(idx, X.Count-idx-1); ++k)
+            for (k = 1; k <= Math.Min(idx, X.Count-idx-1); ++k)
             {
                 below = Add_Below(idx+k);
                 above = Add_Above(idx-k);
                 Gauss.AddRange(below.TakeLast(2));
             }
 
-            for (int k = 0; k < Gauss.Count; ++k)
+            for (int i = 0; i < Gauss.Count; ++i)
             {
-                Gauss[k] = Gauss[k]/Enumerable.Range(1, k).Aggregate(1, (p, item) => p*item);
+                Gauss[i] = Gauss[i]/Enumerable.Range(1, i).Aggregate(1, (p, item) => p*item);
             }
+
+            /* Xây dựng ma trận hệ số t */
+            t_table = Program.Construct_coeff_matrix(2*k-1);
+
+            res = Program.matMul(Gauss, t_table);
+            // res.ForEach(x => Console.Write(x + " "));
+            // Console.WriteLine();
+
+            return res;
         }
+        
+        public void P(List<double> pol, double x, int idx)
+        {
+            double t = (x - X[idx])/h;
+            double res = 0;
+
+            foreach (var elm in pol)
+            {
+                res = res*t + elm;
+            }
+            Console.WriteLine(res);
+        }
+    }
+
+    class Program
+    {
         static public int[,] Construct_coeff_matrix(int numberOfX)
         {
             int n = numberOfX;
@@ -99,15 +128,6 @@ namespace Week05
 
             return matrix;
         }
-
-        public void Display()
-        {
-            Gauss.ForEach(x => Console.Write(x + " "));
-        }
-    }
-
-    class Program
-    {
         static public List<double> matMul(List<double> lst, int[,] arr)
         {
             int n = lst.Count();
@@ -122,16 +142,16 @@ namespace Week05
             }
             return temp;
         }
+        
         static void Main(string[] args)
         {
             string inpPath = @".\input.txt";
-            CentralInterpolation p = new CentralInterpolation(inpPath);
-            List<double> res = new List<double>();
-            p.Perform_1st_Gauss(2);
-            int[,] table = CentralInterpolation.Construct_coeff_matrix(5);
+            CentralInterpolation user = new CentralInterpolation(inpPath);
+            var pol = user.Perform_1st_Gauss(2);
 
-            res = matMul(p.Gauss, table);
-            res.ForEach(x => Console.Write(x + " "));
+            pol.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            user.P(pol, 2, 2);
         }
     }
 }
