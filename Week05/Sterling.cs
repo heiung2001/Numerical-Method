249eed1892e3f0d66a62481a5143925c1c67e44e
heiung2001 <hieu.working2001@gmail.com>
2021-11-02 12:28:57 +0700

    initial Sterling class

---
+++ Week05/Sterling.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Week05
+{
+    class Sterling : CentralInterpolation
+    {
+        public Sterling(string filePath) : base(filePath) {}
+        
+    }
+}
